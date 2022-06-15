586b60665909c1f961be5f3b3248c6c90aa353ef
OtvCng <otavionogueira@hotmail.com>
2022-06-15 09:09:28 -0300

    s

--- matriz2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def menu_matriz():
-    a = [np.array()]
-    qtd = int(input('Digite a quantidade de linhas da matriz: '))
-    coluna = int(input('Digite a quantidade de colunas da matriz: '))
-    for i in range(qtd):
-        a.append([])
-        
-menu_matriz()
