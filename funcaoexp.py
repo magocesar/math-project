e854184a1ccf9c2aaf39184774a61a170eb74b64
RodrigoMunch <munchrodrigo@gmail.com>
2022-06-15 08:37:45 -0300

    Update funcaoexp.py

--- funcaoexp.py
+++ funcaoexp.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from menu import menu
 
-def tipo(a, b, ):
+def tipo(a, b):
     a = int(input('Qual o valor de "a" : '))
     b = int(input('Qual o valor de "b" :'))
-    if b < 0 or b == 1 :
+    if b < 0 or b == 1 or a == 0:
          print('Função não existe !!')
     else:
         if 0<b<1 :
@@ -17,7 +17,7 @@ def tipo(a, b, ):
 def calcula_funcao(a, b, x):
     a = int(input('Qual o valor de "a" : '))
     b = int(input('Qual o valor de "b" :'))
-    if b < 0 or b == 1 :
+    if b < 0 or b == 1 or a == 0 :
          print('Função não existe !!')
     else:
         x = int(input('Digite o valor de "x" desejado :'))
@@ -28,7 +28,7 @@ def calcula_funcao(a, b, x):
 def gerar_graph(a, b, x):
     a = int(input('Qual o valor de "a" : '))
     b = int(input('Qual o valor de "b" :'))
-    if b < 0 or b == 1 :
+    if b < 0 or b == 1 or a == 0:
          print('Função não existe !!')
     else:
         x1 = np.arange(0, b ** x, 1)
@@ -53,7 +53,7 @@ def funcaoexponencial():
                 if opt != [1, 2, 3, 4]:
                     print('\033[31mERRO!\033[m')
                 elif opt == '4':
-                    return menu()
+                    menu()
                 elif opt == 1:
                     tipo()
                 elif opt == 2:
